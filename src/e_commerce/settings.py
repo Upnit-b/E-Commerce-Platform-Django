08bd4df580f5c824cff92bbfa33a45bd3dc759e7
Upnit-b <upnit.singh@gmail.com>
2026-01-16 20:35:32 -0500

    deployment error for allowed hosts fix

--- src/e_commerce/settings.py
+++ src/e_commerce/settings.py
@@ -15,10 +15,11 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'dev-only-change-me')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(
-    ",") if os.getenv("CSRF_TRUSTED_ORIGINS") else []
+# CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(
+#     ",") if os.getenv("CSRF_TRUSTED_ORIGINS") else []
 
-ALLOWED_HOSTS = [".onrender.com"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split(",")
+
 
 
 # Application definition
